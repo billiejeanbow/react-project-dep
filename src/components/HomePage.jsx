1c83dd5fd264438650bdff55d6f07c7d97468c87
Billie Jean Bowdery <billie-jean.bowdery@dci.education>
2023-11-03 10:27:25 +0100

    updated friday

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -3,8 +3,8 @@ import { Link } from 'react-router-dom';
 function HomePage() {
     return (
         <div className='home-main'>
-            <h1>Welcome to 
-               <br />Got Hoops? </h1>
+            <h2>Welcome to </h2>
+            <h1>Got Hoops?</h1>
             <h2>Search for your favourite NBA players stats.</h2>
             <Link to="/login"> <button> Login</button></Link>
           
